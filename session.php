150a066989b06a7beb36f7c267f93f8b60cdaa9c
Milton M <milton_mungin@yahoo.com>
2016-07-01 15:31:02 -0400

    Minor change to session.php

--- session.php
+++ session.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 include_once 'shoppingcart.php';
-$db_username="root";
-$db_password="root";
+$db_username="sqlusernamehere";
+$db_password="sqlpasswordhere";
 $db_database="mystore";
 
 $conn = mysqli_connect("localhost", $db_username, $db_password, $db_database)or die ("cannot connect to the database");
